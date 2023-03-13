0d1fed2271a5bd3e38b3e195bbcdf3e16c2de573
rudi3k <86956340+rudi3k@users.noreply.github.com>
2023-03-13 01:04:29 +0100

    initial jmc cask, generated

---
+++ Casks/ruk-jmc.rb
@@ -0,0 +1,11 @@
+cask "ruk-jmc" do
+  version "8.3.0,05"
+  sha256 "0dbd05efe465c42b6b9ebdb5221c8e4e992ef23650b10a3009b7622167fc2dd7"
+
+  url "https://download.java.net/java/GA/jmc8/05/binaries/jmc-8.3.0_osx-aarch64.tar.gz"
+  name "ruk-jmc"
+  desc ""
+  homepage ""
+
+  app ""
+end
